49c9a189ba3307d9df9440bc17074fcb98a290ba
Edward <2510319@dundee.ac.uk>
2023-11-19 15:55:09 +0000

    Works with reading in txt file and adding the two numbers. Hasn't been tested with the extra bits that it gets run with.

--- baby_simulator.h
+++ baby_simulator.h
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <bitset>
+using namespace std;
 
 class BabySimulator {
 public:
@@ -11,10 +14,18 @@ public:
     void run();
     int getLineNumber();
     int getFunctionNumber();
+    void getNegation();
+    void printMemory();
 
 
 private:
     // Add necessary data members for simulation
-    unsigned int instruction = 0b10010000000010100000000000000000; //max number is 2^32 - 1, also, number has lsb on the left most.
+    unsigned int memory[32];
+    //function codes are in order from lsb left.
+    unsigned int function_codes[8] = {0b00000000000000000000000000000000,0b00000000000000000010000000000000,0b00000000000000000100000000000000,0b00000000000000000110000000000000,0b00000000000000001000000000000000,0b00000000000000001010000000000000,0b00000000000000000110000000000000,0b000000000000000001110000000000000};
+    unsigned int PI; //Stores the current instruction.
+    unsigned int CI; //5 bit number. To store the location of the current instruction. 
+    unsigned int accumulator;
+
 };
 
